7f1b3ff5bec23f600af973d67564b3c078cf549f
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2023-09-23 15:58:47 -0300

    Config

---
+++ store/src/main/java/com/aec/store/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package com.aec.store.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepo
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> null
+    }
+}
